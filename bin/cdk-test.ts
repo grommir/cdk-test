dfa13c3621481e7f83db59c8e3013688293ab703
Alex <grommir@gmail.com>
2023-09-06 16:42:05 +0300

    add route53 record

--- bin/cdk-test.ts
+++ bin/cdk-test.ts
@@ -3,8 +3,6 @@ import "source-map-support/register";
 import * as cdk from "aws-cdk-lib";
 import * as ec2 from "aws-cdk-lib/aws-ec2";
 
-
-
 import { infrastructureBase } from "../lib/infra-stack";
 import { ec2Instance } from "../lib/ec2-stack";
 import { localZone } from "../lib/route53-local-stack";
@@ -29,9 +27,5 @@ new ec2Instance(app, `${env}-worker`, {
   vpc: infraBase.vpc,
   machineImage: new ec2.AmazonLinuxImage({ generation: ec2.AmazonLinuxGeneration.AMAZON_LINUX_2 }),
   instanceType: ec2.InstanceType.of(ec2.InstanceClass.BURSTABLE2, ec2.InstanceSize.MICRO),
+  route53Zone: localZonesite.localZone
 });
-
-
-
-
-
--- lib/ec2-stack.ts
+++ lib/ec2-stack.ts
@@ -1,11 +1,13 @@
 import * as cdk from "aws-cdk-lib";
 import * as ec2 from "aws-cdk-lib/aws-ec2";
+import { aws_route53 as route53} from "aws-cdk-lib";
 
 export interface CustomProps {
   env: string;
   vpc: ec2.Vpc;
   machineImage: ec2.IMachineImage;
   instanceType: ec2.InstanceType;
+  route53Zone: route53.HostedZone
 }
 
 export class ec2Instance extends cdk.Stack {
@@ -17,6 +19,12 @@ export class ec2Instance extends cdk.Stack {
       vpc: props.vpc,
       instanceType: props.instanceType,
       machineImage: props.machineImage,
+    });
+
+    new route53.ARecord(this, 'ARecord', {
+      zone: props.route53Zone,
+      recordName: `${id}.${props.env}.local`,
+      target: route53.RecordTarget.fromIpAddresses(this.ec2Instance.instancePrivateIp),
     });
 
   }
